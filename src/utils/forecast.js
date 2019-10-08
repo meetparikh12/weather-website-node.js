5c38a0b0455fb15513de69c1f87bbfa9f26c04c5
meetparikh12 <parikhmeet1234@gmail.com>
2019-10-08 22:55:20 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ request({
     }else if(body.error){
         callback('Unable to find location. Try another search.', undefined)
     }else {
-        callback(undefined,body.daily.data[0].summary +' It is currently ' +body.currently.temperature +' degrees out. There is ' +body.currently.precipProbability +'% chances of rain.')
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is ' + body.currently.precipProbability + '% chance of rain.')
     }
 })
 }
